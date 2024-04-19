d829064fe8a76c4cd2b6a54441c9220008911b10
henry <123560600+xxxxxxx1212@users.noreply.github.com>
2024-04-19 09:03:03 -0300

    ajustes

--- Alimentos.c
+++ Alimentos.c
@@ -1,14 +1,18 @@
-static int codigosAlimentos[5] = {21, 22, 23, 24, 25};
-static char nomeProdutoAlimentos[5][50] =
+static int codigosAlimentos[9] = {21, 22, 23, 24, 25, 26, 27, 28, 29};
+static char nomeProdutoAlimentos[9][50] =
 {
     {"Cafe"},
     {"Leite (cx)"},
     {"Arroz (1kg)"},
     {"Feijao"},
-    {"Acucar (1kg)"}
+    {"Acucar (1kg)"},
+    {"Sal"},
+    {"Macarrao"},
+    {"Azeite"},
+    {"Farinha (1kg)"}
 };
-static float precoAlimentos[5] = {19.99, 5.99, 4.5, 8.00, 5.00};
-static int repeticaoAlimentos = 5;
+static float precoAlimentos[9] = {19.99, 5.99, 4.5, 8.00, 5.00, 2.50, 3.00, 15.00, 3.50};
+static int repeticaoAlimentos = 9;
 
 //Funcao que recebe o codigo, reenvia para a funcao de pegar a quantidade, e retorna o preco correspodente
 static float retornoPrecoAlimentos(int codigoCompra)
@@ -38,7 +42,22 @@ static float retornoPrecoAlimentos(int codigoCompra)
         preco = precoAlimentos[4];
         quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
         break;
-    default:
+    case 26:
+        preco = precoAlimentos[5]; 
+        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
+        break;
+     case 27:
+        preco = precoAlimentos[6]; 
+        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
+        break;
+     case 28:
+        preco = precoAlimentos[7]; 
+        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
+     case 29:
+        preco = precoAlimentos[8]; 
+        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
+        break;
+         default:
         preco = 0.0;
         break;
     }
